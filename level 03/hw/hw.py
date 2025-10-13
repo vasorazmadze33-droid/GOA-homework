f98d9274d48af8cfafb501495a49e53448effad7
vasorazmadze33-droid <vasorazmadze33@gmail.com>
2025-10-13 23:26:19 +0400

    davaleba

---
+++ level 03/hw/hw.py
@@ -0,0 +1,20 @@
+# contacination --> 2 ცალი სტრინგის ერთმანეთში შერთებას ჰქვია კონკატინაცია.
+num1 = 14
+num2 = 21
+num3 = 42
+print(num1+num2+num3)
+print(15+21)
+print(51+27)
+print(-3-7)
+print(47-21)
+print(5*7)
+print(6*9)
+print(21/3)
+print(22//3)
+print("me " + "vcxovrob " + "Tbilisshi")
+length = 10
+width = 20 
+perimeter = length + length + width + width
+perimeter = length * 2 + width * 2
+perimeter = (length + width) * 2
+print(perimeter)
