ee7966336ed66c744855cd8fc23b1357fe7f41d0
vasorazmadze33-droid <vasorazmadze33@gmail.com>
2025-10-31 21:21:34 +0400

    დავალება

---
+++ level 08/cw/cw.py
@@ -0,0 +1,12 @@
+name=input("შეიყვანეთ თქვენი სახელი: ")
+print(name)
+
+num1=int(input("2010"))
+num2=int(input("1984"))
+print(num1 + num2)
+
+age = int(input("15"))
+print(type(age))
+text = "გამარჯობა"
+number = 25
+price = 9.99
